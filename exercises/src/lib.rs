6f0fc86968057c4e6092e8d0a616e842ea8dabd6
Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>
2023-02-01 11:56:59 -0500

    feat(lib): combine everything in the exposed API

--- exercises/src/lib.rs
+++ exercises/src/lib.rs
@@ -3,14 +3,16 @@ use std::collections::HashMap;
 pub mod aminoacid;
 pub mod chap01;
 pub mod codon;
+pub mod exercise;
 pub mod nucleotide;
 
 pub use aminoacid::AminoAcid;
 pub use codon::Codon;
+pub use exercise::Exercise;
 pub use nucleotide::Nucleotide;
 
-pub fn solutions() -> HashMap<String, fn()> {
-    let mut solutions: HashMap<String, fn()> = HashMap::new();
-    solutions.insert("1.1.6".to_string(), chap01::e1_1_6::solve);
+pub fn solutions() -> HashMap<String, Box<dyn Exercise>> {
+    let mut solutions: HashMap<String, Box<dyn Exercise>> = HashMap::new();
+    solutions.insert("1.1.6".to_string(), Box::new(chap01::e1_1_6::Solution));
     solutions
 }
