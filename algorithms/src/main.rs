ab9c8e72d7d3c700c7361033dc5f61e7d9c65673
Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>
2023-02-01 11:57:59 -0500

    feat(cli): update cli to respond correctly

--- algorithms/src/main.rs
+++ algorithms/src/main.rs
@@ -8,12 +8,22 @@ use cli::Commands;
 fn main() {
     let cli = Cli::parse();
 
+    let solutions = exercises::solutions();
+
     match cli.command {
         Some(Commands::Test { exercise }) => {
             println!("Test command with Exercise {}", exercise);
+            let solution = solutions.get(&exercise).unwrap();
+            let input = solution.read_input(exercise);
+            let output = solution.solve(input);
+            println!("Output: {}", output);
         }
         Some(Commands::Solve { exercise }) => {
-            println!("Solve command with  Exercise {}", exercise);
+            println!("Solve command with Exercise {}", exercise);
+            let solution = solutions.get(&exercise).unwrap();
+            let input = solution.read_input(exercise);
+            let output = solution.solve(input);
+            println!("Output: {}", output);
         }
         None => {
             println!("No command provided");
